8090a5c3453e75b040e450af2ecb72b825615e30
Dylan <133868326+Idntknwwt2do@users.noreply.github.com>
2024-10-15 13:10:52 +0100

    Finished Title screen
    Main screen, login screen, account creation and password reset screen aswell as the TOS screen and a loading screen with a loading bar, all screens having opening and closing animations. TOS screen scrolls etc

---
+++ Assets/Scripts/SliderIncrease.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderIncrease : MonoBehaviour
+{
+    //made with the assistance of AI. Wrote all the code by myself and comments but using ai as reference
+public Slider loadingbar;
+public float mintime = 1f;
+public float maxtime = 10f;
+public TextMeshProUGUI percentageText;
+void Start()
+{
+    loadingbar.onValueChanged.AddListener((v) => {
+        percentageText.text = v.ToString("0%");
+    });
+    StartCoroutine(Increaseslidervalue());
+    
+}
+IEnumerator Increaseslidervalue() //IEnumerator doesn't interrupt gameflow though this is for a loadingscreen 
+{
+    while (loadingbar.value < loadingbar.maxValue) //continues this function while the slidervalue is less than the max value of the slider
+    {
+        float randomTime = Random.Range(mintime, maxtime); //Chooses a random number between the mintime and maxtime floats and assigns it to the float randomTime
+        float initialValue = loadingbar.value; //sets initialValue as the value of the slider
+        for (float t = 0; t < randomTime; t += Time.deltaTime) //For loop, as long as t is less than the randomTime float then it will keep increasing the value.
+        {
+           loadingbar.value = Mathf.Lerp(initialValue, loadingbar.maxValue, t / randomTime); //gradually changes slider value
+           yield return null; //waits until next frame
+        }
+    }
+}
+}
