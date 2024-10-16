1279ccbfef70424dbea0bb1c93ef6b5903ba4234
Dylan <133868326+Idntknwwt2do@users.noreply.github.com>
2024-10-16 13:10:01 +0100

    Lobby menu button progress
    Made the lobby buttons move down when another is selected, the way i did this was really inefficient, should improve later.

--- Assets/Scripts/LobbyScreen/ButtonScriptLobbyScreen.cs
+++ Assets/Scripts/LobbyScreen/ButtonScriptLobbyScreen.cs
@@ -11,29 +11,56 @@ public class ButtonScriptLobbyScreen : MonoBehaviour
     public GameObject LButton;  
     public GameObject SButton;
     public GameObject CButton;
+    private GameObject ActiveButton;
     // Start is called before the first frame update
    public void PbuttonRaise()
    {
-    PButton.transform.DOLocalMoveY(-500, .25f, true);
+    ActiveButton = PButton;
+    ActiveButton.transform.DOLocalMoveY(-500, .25f, true);
    }
    public void QbuttonRaise()
    {
     QButton.transform.DOLocalMoveY(-500, .25f, true);
+    PButton.transform.DOLocalMoveY(-522, .25f, true);
+    RPButton.transform.DOLocalMoveY(-522, .25f, true);
+    LButton.transform.DOLocalMoveY(-522, .25f, true);
+    SButton.transform.DOLocalMoveY(-522, .25f, true);
+    CButton.transform.DOLocalMoveY(-522, .25f, true);
    }
    public void RPbuttonraise()
    {
     RPButton.transform.DOLocalMoveY(-500, .25f, true);
+    QButton.transform.DOLocalMoveY(-522, .25f, true);
+    PButton.transform.DOLocalMoveY(-522, .25f, true);
+    LButton.transform.DOLocalMoveY(-522, .25f, true);
+    SButton.transform.DOLocalMoveY(-522, .25f, true);
+    CButton.transform.DOLocalMoveY(-522, .25f, true);
    }
    public void LbuttonRaise()
    {
     LButton.transform.DOLocalMoveY(-500, .25f, true);
+    QButton.transform.DOLocalMoveY(-522, .25f, true);
+    RPButton.transform.DOLocalMoveY(-522, .25f, true);
+    PButton.transform.DOLocalMoveY(-522, .25f, true);
+    SButton.transform.DOLocalMoveY(-522, .25f, true);
+    CButton.transform.DOLocalMoveY(-522, .25f, true);
    }
    public void Sbuttonraise()
    {
     SButton.transform.DOLocalMoveY(-500, .25f, true);
+    QButton.transform.DOLocalMoveY(-522, .25f, true);
+    RPButton.transform.DOLocalMoveY(-522, .25f, true);
+    LButton.transform.DOLocalMoveY(-522, .25f, true);
+    PButton.transform.DOLocalMoveY(-522, .25f, true);
+    CButton.transform.DOLocalMoveY(-522, .25f, true);
    }
    public void Cbuttonraise()
    {
     CButton.transform.DOLocalMoveY(-500, .25f, true);
+    QButton.transform.DOLocalMoveY(-522, .25f, true);
+    RPButton.transform.DOLocalMoveY(-522, .25f, true);
+    LButton.transform.DOLocalMoveY(-522, .25f, true);
+    SButton.transform.DOLocalMoveY(-522, .25f, true);
+    PButton.transform.DOLocalMoveY(-522, .25f, true);
    }
 }
